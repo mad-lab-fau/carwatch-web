50d05211d02ce7e73203e6647b34add80c2cd39b
Annika Mücke <annika-muecke@outlook.de>
2023-04-25 10:44:36 +0200

    add some styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 
-	import { AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="flex h-full">
@@ -14,10 +14,18 @@
 		<AppRailTile tag="a" href="/" label="Home" value={-1}>(icon)</AppRailTile>
 		<AppRailTile tag="a" href="/preparation" label="Barcode Generator" value={0}>(icon)</AppRailTile>
 		<AppRailTile tag="a" href="/postprocessing" label="Test" value={1}>(icon)</AppRailTile>
-
+		<svelte:fragment slot="trail">
+							<AppRailTile>
+								<LightSwitch/>
+							</AppRailTile>
+		</svelte:fragment>
 	</AppRail>
+	
 	<div class="w-full overflow-auto">
 		<slot/>
 	</div>
 	
 </div>
+
+
+
