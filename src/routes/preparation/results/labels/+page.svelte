c6653c40784d0ec778fa084b5c2dc3b8f3ddbcd7
Annika Mücke <annika-muecke@outlook.de>
2023-04-26 18:20:54 +0200

    add example layout for default avery labels

--- src/routes/preparation/results/labels/+page.svelte
+++ src/routes/preparation/results/labels/+page.svelte
@@ -8,21 +8,39 @@
         -moz-box-sizing: border-box;
     }
     .page {
+        /*A4 format*/
         width: 210mm;
         min-height: 297mm;
-        padding: 20mm;
-        margin: 10mm auto;
-        border: 1px #D3D3D3 solid;
-        border-radius: 5px;
+        /*distance between pages*/
+        margin: 20mm; 
+        /*page margins*/
+        padding-right: 7.3mm; /*true margin right - col dist*/
+        padding-left: 9.8mm;
+        padding-top: 21.2mm;
+        padding-bottom: 21.2mm; /*true margin bottom - row dist*/
         background: white;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
     }
-    .subpage {
+    /*
+    .subpage { 
         padding: 1cm;
         border: 5px red solid;
         height: 257mm;
         outline: 2cm #FFEAEA solid;
-    }
+    }*/
+    .label {
+            /*padding of label content*/
+            padding: 10px;
+            margin-right: 2.5mm;
+            margin-bottom: 0mm;
+            /* height = a4_height - top_margin - bottom_margin - inter_row * (num_rows - 1) / num_rows */
+            /* width = a4_width - left_margin - right_margin - inter_col * (num_cols - 1) / num_cols */
+            width: 44.9mm; /*why doesnt it fit??*/
+            height: 21.2mm;
+            display: inline-block;
+            text-align: left;
+            background: #FFFFFF;
+            outline: 2px dotted #000000;
+        }
     
     @page {
         size: A4;
@@ -44,11 +62,155 @@
             box-shadow: initial;
             background: initial;
         }
+        
     } 
   </style>
     <div class="overflow-visible">
         <div class="page">
-            <div class="subpage">Page 1/2</div>    
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
+            <div class="label">
+                test
+            </div>
         </div>
         <div class="page">
             <div class="subpage">Page 2/2</div>    
