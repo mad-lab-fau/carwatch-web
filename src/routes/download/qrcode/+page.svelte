0b0555e6ad10214ef467ba760a0d3ec5d8697e4b
Annika Mücke <annika-muecke@outlook.de>
2023-06-26 14:07:31 +0200

    use alternative qr package with rendering to image

--- src/routes/download/qrcode/+page.svelte
+++ src/routes/download/qrcode/+page.svelte
@@ -1,65 +1,33 @@
 <script lang="ts">
 	import { studyProps } from "$lib/configStore";
 	import { QR_PER_PAGE } from "$lib/constants";
-    import { qr } from 'headless-qr';
 	import { qrData } from "$lib/dataStore";
 	import { onMount } from "svelte";
+    import QRCode from 'qrcode'; 
 
-
-    let m: boolean[][]; 
+    let dataUrl: string;
 
     onMount(async() => {
-        m = qr(qrData);
-
-        console.log(m);
-
-        const canvases = document.getElementsByClassName('qr');
-
-        Array.from(canvases).forEach(canvas => {
-            if(canvas instanceof HTMLCanvasElement){
-            const ctx = canvas.getContext('2d');
-
-                if(ctx){
-                    const width = m[0].length;
-                    const height = m.length;
-                    console.log("width " + width + " height " + height);
-                    canvas.width = width;
-                    canvas.height = height;
-
-                    for (let y = 0; y < height; y++) {
-                        for (let x = 0; x < width; x++) {
-                        const pixelColor = m[y][x] ? 'black' : 'white';
-                        ctx.fillStyle = pixelColor;
-                        ctx.fillRect(x, y, 1, 1);
-                        }
-                    }
-            ctx.scale(5,5);
+        dataUrl = QRCode.toDataURL($qrData, function(_:any,url: string) {
+            Array.from(document.getElementsByClassName("myqr")).forEach(img => {
+                if (img instanceof HTMLImageElement){
+                    img.src = url;
                 }
-            
-            }
+            });
         });
-    });
+      });
     
     let numPages: number = Math.ceil($studyProps.numSubjects/QR_PER_PAGE);
-    let cellsPerPage = 20;
-   
+    let cellsPerPage = 12;
 </script>
 
 <div class="h-full overflow-y-auto overflow-x-auto">
     {#each Array(numPages) as _, page}
-        <div class="page grid grid-cols-4 bg-white" style:gap="5mm 5mm" style:padding="20mm">
+        <div class="page grid grid-cols-3 bg-white" style:gap="5mm 5mm" style:padding="20mm">
             {#each Array(cellsPerPage) as _, i}
-                {#if !(page*cellsPerPage + i >= $studyProps.numSubjects)}
                 <div class="label p-2 overflow-hidden" >
-                    <div id="canvas-container">
-                        <canvas class="qr"></canvas>
-                    </div>
+                    <img class="myqr object-contain" alt="QR Code"/>
                 </div>
-                {:else}
-                <!-- display empty labels to last page to preserve format -->
-                <div class="label p-2 overflow-hidden">
-                </div>
-                {/if}
             {/each}
         </div>
     {/each}
@@ -79,19 +47,7 @@
         justify-content: center;
         outline: 2px #000000 dotted;
     }
-
-    .label svg {
-        max-width: 100%;
-        max-height: 90%;
-    }
-    .label p {
-        max-width: 100%;
-        max-height: 100%;
-        position: absolute;
-        font-family: monospace, monospace;
-        font-size: xx-small;
-    }
- 
+   
     .page {
         /*A4 format*/
         width: 210mm;
