d0997dc10c9f1fad6c6c6832f07bb82deea44884
Annika Mücke <annika-muecke@outlook.de>
2023-06-26 15:39:32 +0200

    fix stored qr data bug

--- src/lib/dataStore.ts
+++ src/lib/dataStore.ts
@@ -15,7 +15,7 @@ if (browser) {
 
 export const barcodeDataArray = storedBarcodeDataArray ? writable<string[]>(JSON.parse(storedBarcodeDataArray)) : writable<string[]>([]);
 export const captionArray = storedCaptionArray ? writable<string[]>(JSON.parse(storedCaptionArray)) : writable<string[]>([]);
-export const qrData = storedQrData ? writable<string>(storedCaptionArray) : writable<string>("");
+export const qrData = storedQrData ? writable<string>(storedQrData) : writable<string>("");
 
 if(browser){
   barcodeDataArray.subscribe((value) => localStorage.storedBarcodeDataArray = JSON.stringify(value));
