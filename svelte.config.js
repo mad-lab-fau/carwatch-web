87ff9d3108ef5e598b3ee93112fcf3c1559efc6f
Annika Mücke <annika-muecke@outlook.de>
2023-08-10 14:02:52 +0200

    modify configuration to deploy to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-export default {
+ 
+const dev = process.argv.includes('dev');
+ 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
-  },
-  preprocess: [vitePreprocess()]
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/mad-lab-fau/carwatch-web',
+    }
+  }
 };
