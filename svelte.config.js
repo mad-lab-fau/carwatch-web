38e85b9606a98c97e71ab5369253630b443d90e1
Annika Mücke <annika-muecke@outlook.de>
2023-08-10 14:34:14 +0200

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
  
 const dev = process.argv.includes('dev');
- 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
     paths: {
       base: dev ? '' : '/mad-lab-fau/carwatch-web',
     }
-  }
+  },
+  preprocess: [vitePreprocess()]
 };
+
+export default config;
