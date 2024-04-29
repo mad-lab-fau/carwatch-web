0b96d01f59bddafd2b36853056e9bfeab609a667
roschro-fau <robert.schroeter@fau.de>
2024-04-29 15:55:43 +0200

    study day is added to barcode label only if the study has more than one day

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -36,6 +36,7 @@
 		let captions = [];
 		let startSample = $studyProps.startSampleFromZero ? 0 : 1;
 		let studyName = $studyProps.studyName;
+		let addStudyDay = $studyProps.numDays > 1;
 		for (let participant = 1; participant <= $studyProps.numParticipants; participant++) {
 			for (let day = 1; day <= $studyProps.numDays; day++) {
 				let lastSampleId = $studyProps.numSamples + startSample + Number($studyProps.hasEveningSample) - 1;
@@ -53,7 +54,11 @@
 					if (sample == lastSampleId && $studyProps.hasEveningSample) {
 						sampleCaption = 'E';
 					}
-					caption += $studyProps.participantList[participant - 1] + '_D' + day + '_' + $studyProps.samplePrefix + sampleCaption;
+					caption += $studyProps.participantList[participant - 1];
+					if (addStudyDay) {
+						caption += '_D' + day;
+					}
+					caption += '_' + $studyProps.samplePrefix + sampleCaption;
 					captions.push(caption);
 					let data = participantString + dayString + sampleString;
 					barcodeData.push(data);
